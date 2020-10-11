5ca1a3d85cc28ce57851af4120382db8375f947e
Andzej-F <70884391+Andzej-F@users.noreply.github.com>
2020-10-11 15:08:46 +0300

    .

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 // header import
 // hero import
 // about import
-
+// services import
 
 // header logic
 // hero logic
 // about logic
+// services logic
+/*
+    main(displayServices, servicesData)
+*/
