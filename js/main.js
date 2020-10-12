b8f57e01c41d0e88ab0cc458c74f3b8b25b6ff3e
Andzej-F <70884391+Andzej-F@users.noreply.github.com>
2020-10-12 15:09:06 +0300

    basic structure update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
 // header import
 // hero import
 // about import
-// services import
+
 
 // header logic
 // hero logic
 // about logic
-// services logic
-/*
-    main(displayServices, servicesData)
-*/
