7f3bd3bdd2cf60e2cfa06fa488109cf9fdbb4563
Andzej-F <70884391+Andzej-F@users.noreply.github.com>
2020-10-13 19:16:04 +0300

    update

--- js/components/services/generateService.js
+++ js/components/services/generateService.js
@@ -2,13 +2,26 @@ function generateService(service) {
     //validacija
 
     //logika
+
+    // <div id="services_block" class="col-4 col-md-6 col-sm-12 box">
+    //     box
+    //             <div class="header">
+    //         <i class="fa fa-pie-chart logo" aria-hidden="true">
+    //             <a href="#" class="link">Web Design</a>
+    //         </i>
+    //     </div>
+    //     <p class="text">“It is not because things are difficult that we do not dare; it is because we do not
+    //                 dare that they are difficult.”</p>
+    // </div>
+
     //<div id="services_block" class="col-4 col-md-6 col-sm-12 box">
-    const HTML = `<div class="header">
-        <i class="fa fa-${service.logo}" aria-hidden="true">
-            <a href="#" class="link">${service.title}</a>
-        </i>
-                </div >
-            <p class="text">${service.description}</p>`;
+
+    const HTML = `<div class="col-4 col-md-6 col-sm-12 box">
+                    <span class="fa fa-${service.logo} logo" aria-hidden="true">
+                    <a href="#" class="link">${service.title}</a>
+        	        </span>
+                    <p class="text">${service.description}</p>
+                </div >`;
     // output
     return HTML;
 }
