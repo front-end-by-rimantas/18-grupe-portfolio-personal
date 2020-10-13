bbd1eb44726acd1fde3a042d88bacda59e51bcaf
IevaRugine <70884312+IevaRugine@users.noreply.github.com>
2020-10-13 13:49:45 +0300

    updated text with 'import' and 'logic'

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-// header
-// hero
-// about us
-// our services
-// achievements
-// Featured projects
-// Feedback about me
-// blog
-// Sponsors logo
-// footer
+// header import
+// hero import
+// about us import
+// our services import
+// achievements import
+// Featured projects import
+// Feedback about me import
+// blog import
+// Sponsors logo import
+// footer import
 
-// header
-// hero
-// about us
-// our services
-// achievements
-// Featured projects
-// Feedback about me
-// blog
-// Sponsors logo
-// footer
+// header logic
+// hero logic
+// about us logic
+// our services logic
+// achievements logic
+// Featured projects logic
+// Feedback about me logic
+// blog logic
+// Sponsors logo logic
+// footer logic
