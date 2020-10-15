d014074342a64b12379afcb44264bba0a6a685c1
Slashass <70883938+Slashass@users.noreply.github.com>
2020-10-15 19:18:35 +0300

    header needed mobile responsive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 // header import
+import { Header } from './components/header.js';
 // hero import
 // about import
 
 
 // header logic
+new Header({
+    selector: '#main_header'
+});
 // hero logic
 // about logic
