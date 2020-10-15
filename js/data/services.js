959d7fa3255b43851f960bef4cbf52eee83ccba5
Andzej-F <70884391+Andzej-F@users.noreply.github.com>
2020-10-15 11:39:00 +0300

    .

--- js/data/services.js
+++ js/data/services.js
@@ -6,42 +6,36 @@ const servicesData = {
             logo: 'pie-chart',
             title: 'Web Design',
             description: '“It is not because things are difficult that we do not dare; it is because we do not dare that they are difficult.”',
-            active: true
         },
         {
             id: 2,
             logo: 'laptop',
             title: 'Web Development',
             description: 'If you are an entrepreneur, you know that your success cannot depend on the opinions of others. Like the wind, opinions.',
-            active: true
         },
         {
             id: 3,
             logo: 'camera',
             title: 'Photography',
             description: 'Do you want to be even more successful? Learn to love learning and growth. The more effort you put into improving your skills.',
-            active: true
         },
         {
             id: 4,
             logo: 'picture-o',
             title: 'Clipping Path',
             description: 'Hypnosis quit smoking methods maintain caused quite a stir in the medical world over the last two decades. There is a lot of argument.',
-            active: true
         },
         {
             id: 5,
             logo: 'tablet',
             title: 'Apps Interface',
             description: 'Do you sometimes have the feeling that you’re running into the same obstacles over and over again? Many of my conflicts.',
-            active: true
         },
         {
             id: 6,
             logo: 'rocket',
             title: 'Graphic Design',
             description: 'You’ve heard the expression, “Just believe it and it will come.” Well, technically, that is true, however, ‘believing’ is not just thinking that.',
-            active: true
         },
     ]
 };
