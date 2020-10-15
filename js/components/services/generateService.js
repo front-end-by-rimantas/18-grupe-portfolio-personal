2784397509c1e2c15e99c50f9f3851cf08c006bf
Andzej-F <70884391+Andzej-F@users.noreply.github.com>
2020-10-15 22:01:40 +0300

    edited files

--- js/components/services/generateService.js
+++ js/components/services/generateService.js
@@ -1,18 +1,5 @@
 function generateService(service) {
 
-  // <div id="services_block" class="col-4 col-md-6 col-sm-12 box">
-  //     box
-  //             <div class="header">
-  //         <i class="fa fa-pie-chart logo" aria-hidden="true">
-  //             <a href="#" class="link">Web Design</a>
-  //         </i>
-  //     </div>
-  //     <p class="text">“It is not because things are difficult that we do not dare; it is because we do not
-  //                 dare that they are difficult.”</p>
-  // </div>
-
-  //<div id="services_block" class="col-4 col-md-6 col-sm-12 box">
-
   const HTML = `<div class="col-4 col-md-6 col-sm-12">
                     <div class="single-service">  
                         <i class="logo fa fa-${service.logo}" aria-hidden="true"></i>
