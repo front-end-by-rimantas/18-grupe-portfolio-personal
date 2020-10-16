08a829f8d35a6c643df916c325de6f8da1bf550c
IevaRugine <70884312+IevaRugine@users.noreply.github.com>
2020-10-16 14:24:39 +0300

    render gallery pictures

--- js/components/gallery/formatPhotoPost.js
+++ js/components/gallery/formatPhotoPost.js
@@ -1,13 +1,17 @@
-function formatPhotoPost() {
+function formatPhotoPost(gallery) {
+    console.log(gallery);
 
-    return `     <div class="col-12 gallery">
-    <div class="filter">
-        FILTER
-    </div>
-    <div class="list">
-        LIST
-    </div>
-</div>`
+    return `<div class="col-4 col-sm-12">
+                <div class="list">
+                    <div class="img">
+                        <img src="./img/gallery/${gallery.photo}"
+                        alt="Gallery picture">
+                    </div>
+                    <h3>${gallery.name}</h3>
+                    <p>${gallery.subname}</p>
+                </div>
+            </div>`;
+
 }
 
 export { formatPhotoPost }
