60dc52ad5f2efed8899d5287ddaa9f2a2e9fc999
IevaRugine <70884312+IevaRugine@users.noreply.github.com>
2020-10-20 14:38:38 +0300

    filter html

--- js/data/photos.js
+++ js/data/photos.js
@@ -4,33 +4,39 @@ const photosData = {
         {
             photo: 'p1.jpg',
             name: '2D Vinyl Design',
-            subname: 'Vector'
+            subname: 'Vector',
+            category: ["ALL", "VECTOR"]
 
         },
         {
             photo: 'p2.jpg',
             name: '2D Vinyl Design',
-            subname: 'Vector'
+            subname: 'Vector',
+            category: ["ALL", "RASTER"]
         },
         {
             photo: 'p3.jpg',
             name: 'Creative Poster Design',
-            subname: 'Agency'
+            subname: 'Agency',
+            category: ["ALL", "UI/UX"]
         },
         {
             photo: 'p4.jpg',
             name: 'Embosed Logo Design',
-            subname: 'Portal'
+            subname: 'Portal',
+            category: ["ALL", "PRINTING"]
         },
         {
             photo: 'p5.jpg',
             name: '3D Helmet Design',
-            subname: 'Vector'
+            subname: 'Vector',
+            category: ["ALL", "VECTOR"]
         },
         {
             photo: 'p6.jpg',
             name: '2D Vinyl Design',
-            subname: 'Raster'
+            subname: 'Raster',
+            category: ["ALL", "RASTER"]
         },
     ]
 }
