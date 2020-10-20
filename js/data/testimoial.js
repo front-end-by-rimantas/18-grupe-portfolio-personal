20ef9dd7b5ad0eb49ed41d40da3be59af9a83908
kateryna <kateryna_neholiuk@ukr.net>
2020-10-20 19:05:10 +0300

    demo version html and css

---
+++ js/data/testimoial.js
@@ -0,0 +1,31 @@
+const feedback = {
+    selector: "#clientFeedback", 
+    data : [
+        {
+            id:1,
+            message: 
+            "Do you want to be even more successful? Learn to love learning and growth.The more effort you put into improving your skills, the bigger the payoff you.",
+            name:"Harriet Maxwell",
+            job:"CEO at Google",
+        },
+        {
+            id:2,
+            message: "A purpose is the eternal condition for success. Every former smoker can tell you just how hard it is to stop smoking cigarettes. However.",
+            name:"Carolyn Craig",
+            job:"CEO at Facebook",
+        },
+        {
+            id:3,
+            message: 
+            "Do you want to be even more successful? Learn to love learning and growth.The more effort you put into improving your skills, the bigger the payoff you.",
+            name:"Harriet Maxwell",
+            job:"CEO at Google",
+        },
+        {
+            id:4,
+            message: "A purpose is the eternal condition for success. Every former smoker can tell you just how hard it is to stop smoking cigarettes. However.",
+            name:"Carolyn Craig",
+            job:"CEO at Facebook",
+        }
+    ]
+}
