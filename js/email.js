bf51a126fad93ad95f8f7f96120ee77ea1f4d94a
AlgisP <70894520+AlgisP@users.noreply.github.com>
2020-10-27 14:31:35 +0200

    footer algirdas  11

--- js/email.js
+++ js/email.js
@@ -1,8 +1,6 @@
 function verifyEmail() {
     console.log(document.getElementById('email').value);
     document.getElementById('alert-message').innerHTML = document.getElementById('email').value;
-
-
     document.getElementById("email-submit").addEventListener('click', function () {
         // ivesti el pasto adresa; 
         console.log('Enter e-mail address');
@@ -13,63 +11,69 @@ function verifyEmail() {
 
         if (eMailAddress === '') {
             console.log("ERROR: El pasto adresas negali buti tuscias");
+            document.getElementById("alert-message").innerText = "The e-mail address can not be empty";
         }
 
 
-        let isPoint = false;
-        let isAmpersand = false;
+
 
         if (!eMailAddress.includes(".")) {
-            console.log("yra taskas");
-            document.getElementById("alert-message").innerText = "The address has no dot";
+            console.log("nera tasko");
+            document.getElementById("alert-message").innerText = "The e-mail address must contain a dot";
         }
 
-        // for (let i = 0; i < eMailAddress.length; i++) {
-
-        //     if (eMailAddress[i] === '.') {
-        //         isPoint = true;
-        //     }
-
-
-        //     if (!isPoint) {
-        //         console.log("ERROR: el pasto adrese privalo buti bent vienas taskas");
-        //         break;
-        //     }
-
-        // }
-
-        // turi buti @
-
-
-        for (let j = 0; j < eMailAddress.lenght; j++) {
-
-            if (eMailAddress[j] === '@') {
-                isAmpersand = true;
-            }
+        if (!eMailAddress.includes("@")) {
+            console.log("nera kilpeles");
+            document.getElementById("alert-message").innerText = "The e-mail address must contain ampersand";
         }
-
-        if (!isAmpersand) {
-
-            console.log("ERROR: el pasto adrese privalo buti @");
-
-
-        }
-
-
     });
 
-    // el. pasto adresas negali buti tuscias
+    // for (let i = 0; i < eMailAddress.length; i++) {
+
+    //     if (eMailAddress[i] === '.') {
+    //         isPoint = true;
+    //     }
 
 
+    //     if (!isPoint) {
+    //         console.log("ERROR: el pasto adrese privalo buti bent vienas taskas");
+    //         break;
+    //     }
+
+    // }
+
+    // turi buti @
 
 
-
-    // turi buti bent vienas taskas
-
-
-
-
-
+    /*  for (let j = 0; j < eMailAddress.lenght; j++) {
+ 
+          if (eMailAddress[j] === '@') {
+              isAmpersand = true;
+          }
+      }
+ 
+      if (!isAmpersand) {
+ 
+          console.log("ERROR: el pasto adrese privalo buti @");
+ 
+ 
+      }
+ 
+ 
+  });
+ 
+  // el. pasto adresas negali buti tuscias
+ 
+ 
+ 
+ 
+ 
+  // turi buti bent vienas taskas
+ 
+ 
+ 
+*/
 
 }
+
 export { verifyEmail };
