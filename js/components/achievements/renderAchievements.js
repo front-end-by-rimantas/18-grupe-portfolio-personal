4a592cb4e7ae9bc64d4d607a4ef4dde8701f48f9
IevaRugine <70884312+IevaRugine@users.noreply.github.com>
2020-10-28 12:47:12 +0200

    some fixes

--- js/components/achievements/renderAchievements.js
+++ js/components/achievements/renderAchievements.js
@@ -87,40 +87,4 @@ class RenderAchievements {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function renderAchievements(params) {
-    //params validation
-    // console.log(params);
-
-    //params logic
-    let HTML = '';
-    const achievementsDOM = document.querySelector(params.selector);
-    const achievementsData = params.data;
-    const achievementsCount = achievementsData.length
-
-    for (let i = 0; i < achievementsCount; i++) {
-        const achievement = achievementsData[i];
-        HTML += formatAchievement(achievement);
-    }
-    //post logic validation
-
-    achievementsDOM.innerHTML = HTML;
-
-}
-
 export { RenderAchievements }
