e8ebc7766f088524ceb640e3bd7e245562c8a238
Andzej-F <70884391+Andzej-F@users.noreply.github.com>
2020-11-03 10:47:59 +0200

    sponsors html content with oop

--- js/components/sponsors/Sponsors.js
+++ js/components/sponsors/Sponsors.js
@@ -1,41 +1,48 @@
+"use strict";
+
 class Sponsors {
     constructor(params) {
-        this.DOM = params.DOM;
+        this.selector = params.selector;
         this.data = params.data;
         this.imgPath = params.imgPath;
-
-        this.photosDOM = null;
 
         this.init();
     }
 
     init() {
+        if (!this.isValidSelector()) {
+            return;
+        }
         this.render();
     }
 
+    isValidSelector() {
+        const DOM = document.querySelector(this.selector);
+        if (DOM) {
+            this.DOM = DOM;
+            return true;
+        }
+        return false;
+    }
+
     generateHTML() {
-        let HTML = '';
+        let HTML = "";
 
         for (let item of this.data) {
             let imgAlt = item.imgAlt;
             if (!imgAlt) {
                 imgAlt = `${item.name}`;
             }
-
             HTML += `<div class="single-sponsor">
                         <img class="brand-logo" src="${this.imgPath + item.img}" alt="${imgAlt}">
                     </div>`;
         }
-        const feedDOM = document.getElementById('sponsors_block');
-        HTML = feedDOM;
         return HTML;
     }
 
     render() {
-        this.generateHTML();
 
-        //this.photosDOM = document.querySelectorAll('.brand-logo');
-
+        this.DOM.innerHTML = this.generateHTML();
     }
 }
 
